1ecb70508ad35dc2499b53eeeddbc6fbe07e1e0e
jwke21 <jakevm21@gmail.com>
2023-02-10 10:59:35 -0800

    Updated client with new IP address of EC2 instance

--- a1/a1_client/src/main/java/utils/UC.java
+++ a1/a1_client/src/main/java/utils/UC.java
@@ -5,9 +5,9 @@ public class UC {
   public static final int MAX_SWIPER_ID = 5_000;
   public static final int MAX_SWIPEE_ID = 1_000_000;
   public static final int COMMENT_LEN = 256;
-  public static final int NUM_THREADS = 100;
+  public static final int NUM_THREADS = 150;
   public static final int MAX_REQUEST_ATTEMPTS = 5;
   public static final Integer TARGET_NUM_REQUESTS = 500_000;
-  public static String URL = "http://54.212.204.134/a1_server_war/swipe/";
+  public static String URL = "http://52.12.183.77/a1_server_war/swipe/";
   public static final double MSEC_TO_SECONDS_CONV = 0.001;
 }
