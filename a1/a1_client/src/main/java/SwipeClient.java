b4a630695edacf5b4e627e7acb6ec21b90fe1924
jwke21 <jakevm21@gmail.com>
2023-02-09 17:47:35 -0800

    Updated a1 client

--- a1/a1_client/src/main/java/SwipeClient.java
+++ a1/a1_client/src/main/java/SwipeClient.java
@@ -1,6 +1,3 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.concurrent.CountDownLatch;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.RandomUtils;
@@ -8,24 +5,22 @@ import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.core5.http.HttpEntity;
 import org.apache.hc.core5.http.HttpStatus;
 import org.apache.hc.core5.http.io.entity.StringEntity;
 
 public class SwipeClient {
 
-  public static int completedRequests = 0;
+  public static int successfulRequests = 0;
   public static final int MAX_SWIPER_ID = 5000;
   public static final int MAX_SWIPEE_ID = 1000000;
   public static final int COMMENT_LEN = 256;
   public static final int TARGET_NUM_REQUESTS = 500_000;
-//  public static final int REQUEST_BATCH_SIZE = 50_000;
   public static final int NUM_THREADS = 100;
   public static CountDownLatch remainingRequests;
 
-  public static String URL = "http://10.0.0.58:8080/a1_server_war_exploded/swipe/";
+  public static String URL = "http://54.212.204.134/a1_server_war/swipe/";
 
-  public static synchronized void incCompletedRequests() { completedRequests++; }
+  public static synchronized void incSuccessfulRequests() { successfulRequests++; }
 
   private void sendRequests() throws InterruptedException {
     int reqPerThread = TARGET_NUM_REQUESTS / NUM_THREADS;
@@ -40,26 +35,7 @@ public class SwipeClient {
     }
     remainingRequests.await();
 
-//    // Send requests in batches of 50000 to prevent outbound throttling
-//    int numBatches = 0;
-//    while (completedRequests < TARGET_NUM_REQUESTS) {
-//      numBatches++;
-//      int batchSize =
-//          TARGET_NUM_REQUESTS - completedRequests > REQUEST_BATCH_SIZE ?
-//              REQUEST_BATCH_SIZE : TARGET_NUM_REQUESTS;
-//      // Initialize latch for batch
-//      remainingRequests = new CountDownLatch(NUM_THREADS);
-//      // Create threads
-//      int reqPerThread = batchSize / NUM_THREADS;
-//      for (int i = 0; i < NUM_THREADS; i++) {
-//        new Thread(new Requester(reqPerThread)).start();
-//      }
-//      System.out.println("Request batch size: " + batchSize);
-//      System.out.println("Requests per thread: " + reqPerThread);
-//      remainingRequests.await();
-//      System.out.println("Completed batch " + numBatches);
-//    }
-    System.out.println("Completed requests: " + completedRequests);
+    System.out.println("Completed requests: " + successfulRequests);
   }
 
   public static void main(String[] args) throws InterruptedException {
@@ -70,6 +46,7 @@ public class SwipeClient {
   // ------------------------------ Requester ------------------------------
   public class Requester implements Runnable {
 
+    private static final int MAX_REQUEST_ATTEMPTS = 5;
     public int numRequestsToSend;
     public String swipe;
     public int swiper;
@@ -87,50 +64,56 @@ public class SwipeClient {
       comment = RandomStringUtils.random(COMMENT_LEN, true, false);
     }
 
+    public HttpPost buildPostRequest() {
+      // Set random request values
+      setRequestValues();
+      // Initialize a new HttpPost
+      HttpPost ret = new HttpPost(URL + swipe);
+      // Create json payload
+      String jsonPayload = "{ swiper: " + swiper + ",swipee: " + swipee + ",comment: " + comment + " }";
+      // Add json payload to PostMethod
+      ret.setEntity(new StringEntity(jsonPayload));
+      return ret;
+    }
+
     @Override
     public void run() {
       // Create HttpClient
       CloseableHttpClient client = HttpClients.createDefault();
+      // Send the given number of requests
       for (int i = 0; i < numRequestsToSend; i++) {
-        // Set random request values
-        setRequestValues();
         // Create PostMethod
-        HttpPost httpPost = new HttpPost(URL + swipe);
-        // Create json payload
-        String jsonPayload = "{ swiper: " + swiper + ",swipee: " + swipee + ",comment: " + comment + " }";
-        // Add json payload to PostMethod
-        httpPost.setEntity(new StringEntity(jsonPayload));
-        try {
-          // Execute the method
-          CloseableHttpResponse response = client.execute(httpPost);
-          int statusCode = response.getCode();
-          // Get the response body
-          HttpEntity responseEntity = response.getEntity();
-          // If the request was rejected, print response error info by reading response body
-          if (responseEntity != null && statusCode != HttpStatus.SC_OK) {
-            BufferedReader bufReader =
-                new BufferedReader(new InputStreamReader(responseEntity.getContent()));
-            String line;
-            StringBuilder responseBodyBuilder = new StringBuilder();
-            while ((line = bufReader.readLine()) != null) {
-              responseBodyBuilder.append(line);
+        HttpPost httpPost = buildPostRequest();
+        // Attempt request 5 times
+        for (int attempts = 0; attempts < MAX_REQUEST_ATTEMPTS; attempts++) {
+          try {
+            // Execute the method
+            CloseableHttpResponse response = client.execute(httpPost);
+            // Get response code
+            int statusCode = response.getCode();
+            // Close response object
+            response.close();
+            // If the request was successful break out of loop
+            if (statusCode == HttpStatus.SC_OK) {
+              incSuccessfulRequests();
+              break;
             }
-            String responseBody = responseBodyBuilder.toString();
-            System.err.println("Method failed: " + responseBody);
+            // If request was unsuccessful, try again
+          } catch (Exception e) {
+            // If there was an exception print it and try again
+            System.err.println("POST request error");
+            System.out.println(e);
           }
-          // Close response
-          response.close();
-        } catch (Exception e) {
-          System.err.println("POST request error");
-          System.out.println(e);
-        } finally {
-          incCompletedRequests();
         }
       }
-      // Close client
+      // Close HttpClient
       try {
         client.close();
-      } catch (Exception e) {}
+      } catch (Exception e) {
+        System.err.println("Client close error");
+        System.out.println(e);
+      }
+      // Count down latch at thread termination
       remainingRequests.countDown();
     }
   }
