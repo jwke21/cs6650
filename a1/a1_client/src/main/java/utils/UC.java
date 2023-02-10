67260229a6275b8634d78036dc525a93b5d515ad
jwke21 <jakevm21@gmail.com>
2023-02-10 13:14:44 -0800

    Upadated a1

--- a1/a1_client/src/main/java/utils/UC.java
+++ a1/a1_client/src/main/java/utils/UC.java
@@ -5,7 +5,7 @@ public class UC {
   public static final int MAX_SWIPER_ID = 5_000;
   public static final int MAX_SWIPEE_ID = 1_000_000;
   public static final int COMMENT_LEN = 256;
-  public static final int NUM_THREADS = 200;
+  public static final int NUM_THREADS = 100;
   public static final int MAX_REQUEST_ATTEMPTS = 5;
   public static final Integer TARGET_NUM_REQUESTS = 500_000;
   public static final double MSEC_TO_SECONDS_CONV = 0.001;
