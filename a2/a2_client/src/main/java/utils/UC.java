9e376986c461ebe34ce90279ace19b546e20e968
jwke21 <jakevm21@gmail.com>
2023-03-01 22:41:01 -0800

    a2_client

---
+++ a2/a2_client/src/main/java/utils/UC.java
@@ -0,0 +1,16 @@
+package utils;
+
+public class UC {
+
+    public static final int MAX_SWIPER_ID = 5_000;
+    public static final int MAX_SWIPEE_ID = 1_000_000;
+    public static final int COMMENT_LEN = 256;
+    public static final int NUM_THREADS = 100;
+    public static final int MAX_REQUEST_ATTEMPTS = 5;
+    public static final int REQUEST_SUCCESS = 1;
+    public static final int REQUEST_FAILURE = 2;
+    public static final Integer TARGET_NUM_REQUESTS = 500_000;
+    public static final double MSEC_TO_SECONDS_CONV = 0.001;
+    public static String SWIPE_SERVLET_ADDR = "http://localhost:8080/a2_server_war_exploded/swipe/";
+
+}
---
+++ a2/a2_consumer/src/main/java/consumer/AbstractSwipeConsumer.java
@@ -0,0 +1,93 @@
+package consumer;
+
+import com.google.gson.Gson;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
+import rmq.RmqConnectionHandler;
+import utils.UC;
+import java.nio.charset.StandardCharsets;
+
+public abstract class AbstractSwipeConsumer {
+
+    private String exchangeName;
+    private String queueName;
+    private RmqConnectionHandler connectionHandler;
+    private Thread[] threadPool;
+    private static Gson gson;
+
+    public AbstractSwipeConsumer(String exchangeName, String queueName) {
+        this.exchangeName = exchangeName;
+        this.queueName = queueName;
+        this.connectionHandler = RmqConnectionHandler.createConnectionHandler(UC.NUM_THREADS, UC.RMQ_HOST_ADDRESS);
+        this.gson = new Gson();
+        // Initialize the thread pool
+        this.threadPool = new Thread[UC.NUM_THREADS];
+        for (int i = 0; i < UC.NUM_THREADS; i++) {
+            threadPool[i] = new Thread(new Consumer());
+        }
+    }
+
+    public void consume() {
+        int numThreads = threadPool.length;
+        for (int i = 0; i < numThreads; i++) {
+            threadPool[i].start();
+        }
+    }
+
+    public abstract void updateStats(int swiperId, int swipeeId, boolean liked);
+
+
+    // ------------------------------ Consumer ------------------------------
+    public class Consumer implements Runnable {
+
+        @Override
+        public void run() {
+            // Borrow a channel from the exchange
+            Channel channel = connectionHandler.borrowChannel();
+            try {
+                // Set this channel to receive only one message between ack
+                int prefetchCount = 1;
+                channel.basicQos(prefetchCount);
+
+                // Declare a non-durable queue
+                boolean durableQueue = false;
+                channel.queueDeclare(queueName, durableQueue, false, false, null);
+                // Declare a non-durable exchange
+                boolean durableExchange = false;
+                channel.exchangeDeclare(UC.RMQ_EXCHANGE_NAME, UC.RMQ_EXCHANGE_TYPE, durableExchange);
+                // Bind the queue to the exchange (without a routing key)
+                channel.queueBind(queueName, exchangeName, "");
+
+                // Callback lambda used when message delivered
+                DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                    // Get the msg json body
+                    String msg = new String(delivery.getBody(), StandardCharsets.UTF_8);
+                    SwipeMessageJson json = gson.fromJson(msg, SwipeMessageJson.class);
+
+                    // Update the stats
+                    updateStats(json.swiper, json.swipee, json.like);
+                };
+
+                // Consume any messages that arrive and manually ack them
+                boolean autoAck = true;
+                channel.basicConsume(queueName, autoAck, deliverCallback, consumerTag -> { });
+            } catch (Exception e) {
+                System.err.println("Consumer thread error");
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+    }
+
+    // ------------------------------ SwipeMessageJson ------------------------------
+    public static class SwipeMessageJson {
+        public int swiper;
+        public int swipee;
+        public boolean like;
+    }
+
+    // ------------------------------ UserSwipeStats ------------------------------
+    public static abstract class UserSwipeStats {
+        public abstract String toJson();
+    }
+}
