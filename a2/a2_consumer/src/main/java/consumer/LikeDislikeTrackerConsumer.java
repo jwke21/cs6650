1447c90904c5128902b28a96d81625749c764aa3
jwke21 <jakevm21@gmail.com>
2023-03-01 22:41:12 -0800

    Updated consumer

---
+++ a2/a2_consumer/src/main/java/consumer/LikeDislikeTrackerConsumer.java
@@ -0,0 +1,48 @@
+package consumer;
+
+import utils.UC;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class LikeDislikeTrackerConsumer extends AbstractSwipeConsumer {
+
+    ConcurrentHashMap<Integer, LikeDislikeStats> bySwiperId;
+
+    public LikeDislikeTrackerConsumer() {
+        super(UC.RMQ_EXCHANGE_NAME, UC.RMQ_LIKE_DISLIKE_QUEUE_NAME);
+        bySwiperId = new ConcurrentHashMap<>(UC.MAX_SWIPER_ID);
+        // Initialize the map
+        for (int i = 0; i < UC.MAX_SWIPER_ID; i++) {
+            bySwiperId.put(i, new LikeDislikeStats());
+        }
+    }
+
+    @Override
+    public void updateStats(int swiperId, int swipeeId, boolean liked) {
+        // Atomically update the like/dislike count
+        bySwiperId.computeIfPresent(swipeeId, (k, v) -> {
+            if (liked) {
+                v.numLikes++;
+            } else {
+                v.numDislikes++;
+            }
+            return v;
+        });
+    }
+
+    public static void main(String[] argv) {
+        LikeDislikeTrackerConsumer consumer = new LikeDislikeTrackerConsumer();
+        consumer.consume();
+    }
+
+    // ------------------------------ LikeStats ------------------------------
+    public static class LikeDislikeStats extends AbstractSwipeConsumer.UserSwipeStats {
+        int numLikes;
+        int numDislikes;
+
+        @Override
+        public String toJson() {
+            return new String("{likes:" + numLikes + ",dislikes:" + numDislikes + "}");
+        }
+    }
+}
---
+++ a2/a2_consumer/src/main/java/consumer/LikedUsersTrackerConsumer.java
@@ -0,0 +1,53 @@
+package consumer;
+
+import utils.UC;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+public class LikedUsersTrackerConsumer extends AbstractSwipeConsumer {
+
+    ConcurrentHashMap<Integer, LikedUsersStats> bySwiperId;
+
+    public LikedUsersTrackerConsumer() {
+        super(UC.RMQ_EXCHANGE_NAME, UC.RMQ_LIKED_USERS_QUEUE_NAME);
+        bySwiperId = new ConcurrentHashMap<>(UC.MAX_SWIPER_ID);
+        // Initialize the map
+        for (int i = 0; i < UC.MAX_SWIPER_ID; i++) {
+            bySwiperId.put(i, new LikedUsersStats());
+        }
+    }
+
+    @Override
+    public void updateStats(int swiperId, int swipeeId, boolean liked) {
+        // Atomically update the list of liked users
+        bySwiperId.computeIfPresent(swiperId, (k, v) -> {
+            v.likedUsers.add(swipeeId);
+            return v;
+        });
+    }
+
+    public static void main(String[] argv) {
+        LikedUsersTrackerConsumer consumer = new LikedUsersTrackerConsumer();
+        consumer.consume();
+    }
+
+    // ------------------------------ LikedUsers ------------------------------
+    public static class LikedUsersStats extends AbstractSwipeConsumer.UserSwipeStats {
+        ArrayList<Integer> likedUsers;
+        int total;
+
+        public LikedUsersStats() {
+            likedUsers = new ArrayList<>(100);
+        }
+
+        @Override
+        public String toJson() {
+            // "Given a user id, return a list of 100 users maximum"
+            List<Integer> ret = likedUsers.size() <= 100 ? likedUsers : likedUsers.subList(0, 100);
+            return new String("{likedUsers" + ret + "}");
+        }
+    }
+}
