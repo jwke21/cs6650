6ff8e0b86bb770a2ef23476fdef95b910b58c8df
jwke21 <jakevm21@gmail.com>
2023-03-01 23:27:09 -0800

    Updated RMQ host ip with AWS EC2 instance addr

--- a2/a2_server/src/main/java/utils/UC.java
+++ a2/a2_server/src/main/java/utils/UC.java
@@ -7,11 +7,10 @@ public class UC {
     public static final int MAX_SWIPEE_ID = 1_000_000;
     public static final int COMMENT_LENGTH = 256;
     public static final int NUM_CHANNELS = 100;
-    public static final String RMQ_HOST_NAME = "localhost";
+    public static final String RMQ_HOST_NAME = "54.68.180.97";
     public static final String RMQ_VHOST = "a2_host";
     public static final String RMQ_USERNAME = "admin";
     public static final String RMQ_PASSWORD = "admin666";
-    public static final String SWIPE_QUEUE_NAME = "swipe_queue";
     public static final String RMQ_SWIPE_EXCHANGE_NAME = "swipe_exchange";
     public static final String RMQ_SWIPE_EXCHANGE_TYPE = "fanout";
 }
