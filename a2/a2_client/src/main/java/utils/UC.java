0f9c479385ccc20262f0b5918f9a6ab5da87fe92
jwke21 <jakevm21@gmail.com>
2023-03-03 00:25:54 -0800

    updated client

--- a2/a2_client/src/main/java/utils/UC.java
+++ a2/a2_client/src/main/java/utils/UC.java
@@ -5,10 +5,11 @@ public class UC {
     public static final int MAX_SWIPER_ID = 5_000;
     public static final int MAX_SWIPEE_ID = 1_000_000;
     public static final int COMMENT_LEN = 256;
-    public static final int NUM_THREADS = 100;
+    public static final int NUM_THREADS = 200;
     public static final int MAX_REQUEST_ATTEMPTS = 5;
     public static final Integer TARGET_NUM_REQUESTS = 500_000;
     public static final double MSEC_TO_SECONDS_CONV = 0.001;
-    public static String SWIPE_SERVLET_ADDR = "http://54.185.208.4/a2_swipe_servlet/swipe/";
+    public static String SWIPE_SERVLET_ADDR = "http://swipe-servlet-application-lb-304876359.us-west-2.elb.amazonaws.com/a2_swipe_servlet/swipe/";
+//    public static String SWIPE_SERVLET_ADDR = "http://54.185.208.4/a2_swipe_servlet/swipe/";
 //    public static String SWIPE_SERVLET_ADDR = "http://localhost:8080/a2_server_war_exploded/swipe/";
 }
